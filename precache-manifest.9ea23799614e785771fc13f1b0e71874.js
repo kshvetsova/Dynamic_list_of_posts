c9aaec3c1de8473929befe916a6e9c3b343ff477
kshvetsova <katshvetsova@gmail.com>
2021-03-15 21:37:05 +0200

    Updates

---
+++ precache-manifest.9ea23799614e785771fc13f1b0e71874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4159f9a3c504479dffc002ce750b301a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0eefa3f2ce41bdd467c",
+    "url": "./static/css/main.3063d337.chunk.css"
+  },
+  {
+    "revision": "47081fb6d02175dc2ee0",
+    "url": "./static/js/2.b0023671.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.b0023671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0eefa3f2ce41bdd467c",
+    "url": "./static/js/main.29f60073.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
