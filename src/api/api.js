2aede937e12f76bb1a52b0cf5165070c8a5f1b6f
kshvetsova <katshvetsova@gmail.com>
2021-09-26 06:10:52 +0300

    Initial commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
-const BASE_URL = 'https://mate-api.herokuapp.com/';
+const BASE_URL = 'https://mate.academy/students-api/';
 
 export const request = (url, options) => (
   fetch(`${BASE_URL}${url}`, options)
     .then(response => response.json())
-    .then(res => res.data)
 );
